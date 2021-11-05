52963fbe04176a1f28f877cac5879bc11d618dea
Luke Taylor <99ltaylor@gmail.com>
2021-11-05 11:40:06 +0000

    update code to show api data in server's browser

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,38 @@
-import React from "react";
-import logo from "./logo.svg";
+import './App.css';
+import React, { useState, useEffect } from 'react'
 import Options from './Options'
-import "./App.css";
-
 
 function App() {
-
-  return (
-    <div className= 'App'>
-        <Options/>
-    </div>
-) 
-
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    fetch("/api")
+  let [data, setData] = useState( [] );
+  
+  useEffect(() => {
+    fetch('/api')
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
-
+  // At the moment the useEffect doesn't pull the data 
+  
+    // useEffect(() => {
+    //   fetch("/api")
+    //   .then(response => response.json())
+    //       // 4. Setting *dogImage* to the image url that we received from the response above
+    //   .then(data => setBrand(data))
+    // },[])
+  
   return (
+      
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>{!data ? "Loading..." : data}</p>
       </header>
+
+      <div className= 'App'>
+          <Options/>
+      </div>
     </div>
-  );
+  ) 
+
 }
 
 export default App;
-
